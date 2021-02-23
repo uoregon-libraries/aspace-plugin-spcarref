6b6e5b7d2ed5796919d3628fc0637b3a2de2f979
lsat12357 <lsato@uoregon.edu>
2021-02-23 07:22:15 -0800

    add personhood check

--- public/controllers/questions_controller.rb
+++ public/controllers/questions_controller.rb
@@ -7,17 +7,17 @@ include PrefixHelper
     @request.collection = params['collection']
     @request.question = params['question']
     @request.consulted = params['consulted']
-
+    year = Time.zone.now.strftime('%Y')
     errs = @request.validate
-    if params["comment"].present?
+    if params["comment"].present? ||
+       params["personhood"] != 'n' ||
+       params['current_year'] != year
       errs << I18n.t('request.failed')
     end
     if errs.blank?
       flash[:notice] = I18n.t('request.submitted')
-
       QuestionMailer.question_received_staff_email(@request).deliver
       QuestionMailer.question_received_email(@request).deliver
-
       redirect_to params.fetch('base_url', app_prefix(request[:request_uri]))
     else
       flash[:error] = errs
